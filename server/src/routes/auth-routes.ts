8a2d1bfa2a264cefe2fb7955f0b0b593a780b612
godray2024 <rimonhamo@yahoo.com>
2024-12-11 21:40:25 -0500

    added auth routes

--- server/src/routes/auth-routes.ts
+++ server/src/routes/auth-routes.ts
@@ -1,10 +1,36 @@
+
 import { Router, Request, Response } from 'express';
 import { User } from '../models/user.js';
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 
 export const login = async (req: Request, res: Response) => {
+
   // TODO: If the user exists and the password is correct, return a JWT token
+  const { username, password  } = req.body;
+
+const user = await User.findOne({
+  where: {username},
+});
+
+if (!user) {
+  return res.status(401).json({message: 'Authentication Failed'});
+}
+
+// If the password checks out
+const passwordValid = await bcrypt.compare(password, user.password);
+
+if (!passwordValid) {
+  return res.status(401).json({message: 'Authentication Failed'});
+}
+
+const secretKey = process.env.JWT_SECRET_KEY || '';
+
+// Signs in using the secret key, with expiration of only 1 hour.
+const token = jwt.sign({username}, secretKey, {expiresIn: '1h'});
+
+return res.json({token});
+
 };
 
 const router = Router();
