ce65d8e25b0a539a1bcb08fc7ecd5e6490206864
godray2024 <rimonhamo@yahoo.com>
2024-12-11 21:45:24 -0500

    added auth.ts on client

--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -6,5 +6,27 @@ interface JwtPayload {
 }
 
 export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
-  // TODO: verify the token exists and add the user data to the request object
+  // Gets header authorization information.
+  const authorHeader = req.headers.authorization;
+
+  if (authorHeader) { 
+    const token =  authorHeader.split(' ')[1];
+
+    // Gets key
+    const secretKey = process.env.JWT_SECRET_KEY || '';
+
+    jwt.verify(token, secretKey , (err, user) => {
+      if (err) {
+        return res.sendStatus(403);
+      }
+
+    // Gets User as Payload (str)
+    req.user = user as JwtPayload;
+
+
+    return next();
+  });
+} else {
+  res.sendStatus(402);
+}
 };
